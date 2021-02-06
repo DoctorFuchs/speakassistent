719abb6eebee21aa4a5c6e3bf607b5a6a13c6dba
DoctorFuchs <61468448+DoctorFuchs@users.noreply.github.com>
2021-02-06 17:03:44 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+import time
+
+class speakassist:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+    def log(self, command):
+        pass
+
+    class debug:
+        def __init__(self):
+            pass
+
+        def log(self, text):
+            pass
+
+        def view(self):
+            pass
+
+    class addCommand:
+        def __init__(self):
+            pass
+
+        def speak(self, code):
+            pass
+
+        def trigger(self, words):
+            pass
+
+        def exportCommand(self):
+            pass
+
+        def importCommand(self):
+            pass
